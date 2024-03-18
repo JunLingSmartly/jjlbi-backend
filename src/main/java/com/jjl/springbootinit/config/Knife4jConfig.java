47545f455f004d0cf47a7c322940c45694fbcf17
jinjunling <jinjunling@ultrapower.com.cn>
2024-03-18 16:05:18 +0800

    修改模块标题

--- src/main/java/com/jjl/springbootinit/config/Knife4jConfig.java
+++ src/main/java/com/jjl/springbootinit/config/Knife4jConfig.java
@@ -26,7 +26,7 @@ public class Knife4jConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
                         .title("接口文档")
-                        .description("yubi-backend")
+                        .description("jjlbi-backend")
                         .version("1.0")
                         .build())
                 .select()
