ef482117f2e14375431ac89983443e36057bb6fb
jinjunling <jinjunling@ultrapower.com.cn>
2024-03-18 15:14:18 +0800

    搭建后端模板，实现通用功能实现

---
+++ src/main/java/com/jjl/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.jjl.springbootinit.mapper;
+
+import com.jjl.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Entity
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
